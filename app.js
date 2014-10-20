33489e9391c928ac675c2ae0c724450b20acd38c
Yuri Matsui <yuri@wantedly.com>
2014-10-20 20:51:30 +0900

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var main = function(){
   $('p').click(function(){
    $(this).hide('drop', {direction: 'down'}, 1400); 
    $('h1').delay(1400).hide('drop', {direction: 'down'}, 1700);
-   $('#news').delay(2000).show('drop', {direction: 'down'}, 1900);
+   $('#news').delay(2000).fadeIn(1900);
   })
   
   $('#news').hide();
